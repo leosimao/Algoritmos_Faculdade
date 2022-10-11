af6f0a291b1834d386f32f3fdba311050031e5d4
PratsAgricola <assistente.powerbi@prats.com.br>
2022-10-11 17:29:06 -0300

    Tarefa não finalizada, apenas salvando

--- LearnFunction/FunctionsExercise.py
+++ LearnFunction/FunctionsExercise.py
@@ -1,12 +1,78 @@
+import random as rd
 #Functions
 #1. Crie uma função para pedir um número inteiro ao usuário e retornar ele. Toda vez que você precisar de um número informado pelo usuário, utilize ela. Ela não tem parâmetro e o retorno é o valor digitado pelo usuário já com o tipo inteiro.
 
-def inteiro(value):
-    v = int(value)
+def NumInt ():
+    v = int(input("Digite o número desejado: "))
     return v
 
-vlr = float(input("Digite o valor desejado: "))
+def aleatorio(value):
+    a = rd.randint(1, value)
+    return a
 
-fun = inteiro(vlr)
+def mes(month):
+    mt = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    if(month >= 1 and month <= 12):
+        val = True
+        for i in range(0, len(mt) + 1):
+            if(month - 1 == i):
+                m = print(f"O mês escolhido foi {mt[i]}")
+                return m
+def calculos():   
+    opt = 0
+    while(opt != 5):
+        menu = print(
+        """==== MENU ====
+        1. Função quadrado
+        2. Função do Retangulo
+        3. Função do Triangulo
+        4. Função do Trapézio  
+        5. Sair
+        """)
+        opt = int(input("Digite a opção desejada: "))
+        if(opt == 1):
+            qd = int(input("Escolha um número para ser elevado ao quadrado: "))
+            result = qd ** 2
+            result = print(f"O resultado de {qd} ^ 2 é {result}")
+        elif(opt == 2):
+            print("Para realizar esse calculo precisaremos da Base b e altura h")
+            b = float(input("Base: "))
+            h = float(input("Altura: "))
+            result = b * h
+            result = print(f"A área do retangulo é igual a {result}")
+        elif(opt == 3):
+            print("Para calcular a área do triangulo deve-se digita qual é a base do triangulo e sua altura")
+            b = float(input("Base: "))
+            h = float(input("Altura: "))
+            result = (b * h) / 2
+            result = print(f"A área do triangulo é igual a {result}")
+        elif(opt == 4):
+            print("Para ser calculado a área do trapézio deve-se dar a base maior, base menor e e altura")
+            b1 = float(input("Base Maior: "))
+            b2 = float(input("Base menor: "))
+            h = float(input("Altura: "))
+            result = ((b1 + b2) * h) / 2
+            result = print(f"A área do trapézio é igual a {result}")
+        elif(opt == 5):
+            print("Obrigado por participar!")
+            quit
+    return result       
 
-print(f"A parte inteira de {vlr} é igual a: {fun}")
+valor = NumInt()
+aleat = aleatorio(valor)
+print(f"O valor escolhido foi {valor}")
+print(f"O número aleatório entre 1 e {valor} foi , {aleat}")
+
+print()
+#3. Faça uma função que receba um número inteiro por parâmetro e retorne o mês correspondente ao número. Por exemplo, 2 corresponde a "Fevereiro". Se o número informado não corresponder a um mês (1 a 12), retorne a mensagem “Mês inválido”.
+print("==== RETORNA MES ====")
+val = False
+while(val != True):
+    user = int(input("Digite o número do mês desejado: "))
+    if(user >= 1 and user <= 12):
+        val = True
+        month = mes(user)
+    else:
+        print("Mês invalido tente novamente!")
+
+valor = calculos()
