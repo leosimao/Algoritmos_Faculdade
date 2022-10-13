c86d3c052fbfcc51bf3f22e61a8ae0ba61d81417
leosimao <leonardosc.me@gmail.com>
2022-10-13 11:37:13 -0300

    Adicionando alguns exercicios sobre funcoes

--- LearnFunction/FunctionsExercise.py
+++ LearnFunction/FunctionsExercise.py
@@ -1,4 +1,5 @@
 import random as rd
+from time import sleep as sp
 #Functions
 #1. Crie uma função para pedir um número inteiro ao usuário e retornar ele. Toda vez que você precisar de um número informado pelo usuário, utilize ela. Ela não tem parâmetro e o retorno é o valor digitado pelo usuário já com o tipo inteiro.
 
@@ -47,23 +48,43 @@ def calculos():
             result = (b * h) / 2
             result = print(f"A área do triangulo é igual a {result}")
         elif(opt == 4):
+            t = False
             print("Para ser calculado a área do trapézio deve-se dar a base maior, base menor e e altura")
             b1 = float(input("Base Maior: "))
-            b2 = float(input("Base menor: "))
+            while(t != True):
+                b2 = int(input("Base menor: "))
+                if(b2 > b1):
+                    print(f"A base menor é maior que a Base maior, digite um valor válido para base menor!")
+                else:
+                    t = True
             h = float(input("Altura: "))
             result = ((b1 + b2) * h) / 2
             result = print(f"A área do trapézio é igual a {result}")
         elif(opt == 5):
-            print("Obrigado por participar!")
-            quit
+            result = print("Obrigado por participar!")
     return result       
+
+def fatorial(value):
+    while(value <= 0):
+        print("Número menor ou igual a 0 não é permitido, tente novamente")
+        value = int(input("Digite o valor para que seja calculado o fatorial:"))
+        print()
+    if(value == 1):
+        result = value * 1
+        return result
+    else:
+        for i in range(value - 1, 0, -1):
+            value *= i
+    return value
+
+
 
 valor = NumInt()
 aleat = aleatorio(valor)
 print(f"O valor escolhido foi {valor}")
 print(f"O número aleatório entre 1 e {valor} foi , {aleat}")
-
 print()
+sp(2)
 #3. Faça uma função que receba um número inteiro por parâmetro e retorne o mês correspondente ao número. Por exemplo, 2 corresponde a "Fevereiro". Se o número informado não corresponder a um mês (1 a 12), retorne a mensagem “Mês inválido”.
 print("==== RETORNA MES ====")
 val = False
@@ -74,5 +95,13 @@ while(val != True):
         month = mes(user)
     else:
         print("Mês invalido tente novamente!")
-
+print("=====================")
+print()
+sp(2)
+#4. Faça funções para resolver as equações de área:
 valor = calculos()
+print()
+sp(2)
+fat = int(input("Digite o valor para que seja calculado o fatorial: "))
+rFat = fatorial(fat)
+print(f"O resultado do fatorial de {fat} é igual a {rFat}")
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # 👋🏽Seja Bem Vindo!
 
 Me chamo Leonardo, mas todos me chamam de Simão, atualmente tenho 19 anos e estou cursando Engenharia de Software no IFPR, no campus de Paranavaí! Trabalho com Power BI atualmente fazendo alguns relatórios e dashboards transformando os dados em informações, eu estou me apaixonando nessa área portanto acredito que seja em Data Science que irei me aprofundar, nesse repositório irei me esforçar para colocar toda a minha evolução durante o periodo da faculdade, colocarei tudo, desde algoritmos até estruturando banco de dados e quem sabe alguns relatórios em Power BI. Espero que todos aproveitem esse reposotório e que possam tira de aproveito alguma coisa dele, meu objetivo aqui é compartilhar e aprender, fiquem avontade em comentar e dar opiniões sobre os meus códigos, eu desde já agradeço! Mas é isso um muito obrigado e aproveite!!!
+
+## Procedures!
+
+Provavelmente eu ja dei commit nas procedures que criei finalizando elas, achei interessante como é importante nós sabermos a diferença entre procedures e functions assim conseguimos entender melhor o que o nosso algoritmo ira retornar para o nosso usuário final.
+
+## Functions!
+Ainda falta alguns exercicios de funções para terminamos a lista, porém estou gostando muito do desafio aplicando algumas regras dentro da função e fazendo com que o sub-algoritmo funcione da maneira correta, realmente isso é incrivel!!!
